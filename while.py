8718c51190134cdc69d155390ac28b7431f03e76
YNK1985 <young2da85@naver.com>
2021-11-02 13:45:32 +0900

    a

---
+++ while.py
@@ -0,0 +1,18 @@
+#while.py
+
+while True:
+    score = int(input("enter your score:"))
+
+    if 90 <= score <= 100:
+        grade = "A"
+    elif 80 <=score < 90:
+        grade = "B"
+    elif 70 <=score < 800:
+        grade = "C"
+    else:
+        grade = "D"
+
+    print ("Your grade is",grade,".")
+
+
+
---
+++ while_for-in.py
@@ -0,0 +1,24 @@
+#while & for-in
+
+"""
+value=5
+while value > 0:
+    value-=1
+
+    print(value)
+
+lst=["apple", 100,2.14]
+for item in lst:
+    print(item,type(item))
+"""
+
+#Dict
+d={"apple":100,"orange":200,"banana":300 }
+for key,value in d.items():
+    print(key,value)
+for key in d.keys():
+    print(key)
+for value in d.values():
+    print(value)
+
+
