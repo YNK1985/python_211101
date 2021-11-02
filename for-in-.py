f7d54b51eced90c48c679ddb6bcb19ab590bfc3b
YNK1985 <young2da85@naver.com>
2021-11-02 15:15:17 +0900

    a

--- for-in-.py
+++ for-in-.py
@@ -3,7 +3,8 @@ print("구구단")
 for i in [2,3,4,5,6,7,8,9]:
     print("{0}단".format(i))
     for j in [1,2,3,4,5,6,7,8,9]:
-        print("{0}*{1}={2}".format(i,j,i*j))
+            
+            print("{0}*{1}={2}".format(i,j,i*j))
 
 
 print("break")
