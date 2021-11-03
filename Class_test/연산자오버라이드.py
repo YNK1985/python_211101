4e8ae628fca377a92e3f678f5271ccf3020ccace
YNK1985 <young2da85@naver.com>
2021-11-03 14:46:47 +0900

    override

--- Class_test/연산자오버라이드.py
+++ Class_test/연산자오버라이드.py
@@ -13,3 +13,20 @@ n + 100
 print(n.Num)
 n - 110 
 print(n.Num)
+
+#일반 메소드
+class NumBox:
+	def __init__(self, num):
+		self.Num = num
+	def add(self, num):
+		self.Num += num
+	def sub(self, num):
+		self.Num -= num
+
+#인스턴스 생성
+n = NumBox(40)
+n.add(100) 
+print(n.Num)
+n.sub(110) 
+print(n.Num)
+
