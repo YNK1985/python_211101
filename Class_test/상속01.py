46c2d5b1506b54483dce65587b08c33245ac2297
YNK1985 <young2da85@naver.com>
2021-11-03 15:23:21 +0900

    부모클래스-자식클래스

--- Class_test/상속01.py
+++ Class_test/상속01.py
@@ -1,3 +1,5 @@
+
+#부모클래스 정의
 class Person:
     def __init__(self, name, phoneNumber):
         self.name = name
@@ -6,16 +8,35 @@ class Person:
         print("Info(Name:{0}, Phone Number: {1})".format(
             self.name, self.phoneNumber))
 
+#자식클래스 정의
 class Student(Person):
     def __init__(self, name, phoneNumber, subject, studentID):
-        self.name = name
-        self.phoneNumber = phoneNumber
+        # self.name = name #subject와 Student ID 안나옴 (주석처리 하면 나옴)
+        # self.phoneNumber = phoneNumber #subject와 Student ID 안나옴 (주석처리 하면 나옴)
+        
+        Person.__init__(self,name,phoneNumber) #부모 클래스 껄 그대로 가져다 씀
+        
         self.subject = subject
         self.studentID = studentID
+    def printInfo(self):
+        
+        print("Info(Name:{0}, Phone Number: {1})".format(
+            self.name, self.phoneNumber))
+        print("Info(Subject:{0}, student ID: {1})".format(
+            self.subject, self.studentID))
 
 
 p = Person("전우치", "010-222-1234")
 s = Student("이순신", "010-111-1234", "컴공", "991122")
 p.printInfo()
-s.printInfo()
+print(p.__dict__)
+s.printInfo() 
+print(s.__dict__)
+
+
+print(issubclass(Student,Person))
+print(issubclass(Person,Student))
+print(issubclass(Student,object))
+print(issubclass(Person,object))
+
 
