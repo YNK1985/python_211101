dd855d9d7694b29973399a4e52eddb351eb52008
YNK1985 <young2da85@naver.com>
2021-11-03 13:40:03 +0900

    isinstance

--- Class_test/isinstace함수_파생여부확인.py
+++ Class_test/isinstace함수_파생여부확인.py
@@ -6,8 +6,11 @@ class Student(Person):
     pass
 
 p, s = Person(), Student()
+b=Bird()
+
 print("p is instance of Person: ", isinstance(p, Person))
 print("s is instance of Person: ", isinstance(s, Person))
 print("p is instance of Object: ", isinstance(p, object))
 print("p is instance of Bird: ", isinstance(p, Bird))
+print("b is instance of Bird: ", isinstance(b, Bird))
 print("int is instance of Object: ", isinstance(int, object))
--- Class_test/self누락_GString클래스.py
+++ Class_test/self누락_GString클래스.py
@@ -1,12 +1,13 @@
-str = "Not Class Member"
+str = "Not Class Member" #전역변수
 class GString:
     def __init__(self):
-        self.str = "" 
+        self.str = ""  #인스턴스 멤버 변수 self.str
     def set(self, msg):
         self.str = msg
     def print(self):
-        print(str)
+        print(str) #버그가 발생했다... -> self.str
 
+#인스턴스 (복사본) 생성
 g = GString()
 g.set("First Message")
 g.print()
