7d5923bceb9baaf57772c1893215828ad3c24845
YNK1985 <young2da85@naver.com>
2021-11-04 16:52:25 +0900

    Clien

--- 클리앙중고장터검색.py
+++ 클리앙중고장터검색.py
@@ -1,32 +1,39 @@
 # coding:utf-8
 from bs4 import BeautifulSoup
 import urllib.request
-import re 
+import re
 
 #User-Agent를 조작하는 경우(아이폰에서 사용하는 사파리 브라우져의 헤더) 
 hdr = {'User-agent':'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/603.1.23 (KHTML, like Gecko) Version/10.0 Mobile/14E5239e Safari/602.1'}
 
 for n in range(0,10):
         #클리앙의 중고장터 주소 
-        data ='https://www.clien.net/service/board/sold?&od=T31&po=' + str(n)
+        data ='https://www.clien.net/service/board/sold?&od=T31&po=' + str(n) #주소를 바꾸고
+        print(data)
         #웹브라우져 헤더 추가 
         req = urllib.request.Request(data, \
                                     headers = hdr)
-        req = urllib.request.Request(data)
+        
         data = urllib.request.urlopen(req).read()
-        page = data.decode('utf-8', 'ignore')
+
+        page = data.decode('utf-8', 'ignore') #한글이 깨질경우에 사용
         soup = BeautifulSoup(page, 'html.parser')
-        list = soup.findAll('a', attrs={'class':'list_subject'})
+        
+# <span class="subject_fixed" data-role="list-title-text" title="아이폰11 프로 256 그린, 아이폰 xs 256 화이트(외국판) 팝니다.">
+#                             아이폰11 프로 256 그린, 아이폰 xs 256 화이트(외국판) 팝니다.
+# </span>        
+        
+        #<span> tag 에서 필터링 attributes이 있는 것
+        list = soup.find_all('span', attrs={'data-role':'list-title-text'}) #태그 바꾸고
 
         for item in list:
                 try:
-                        #<a class='list_subject'><span>text</span><span>text</span>
-                        span = item.contents[1]
-                        span2 = span.nextSibling.nextSibling
-                        title = span2.text 
-                        if (re.search('아이폰', title)):
+                    
+                        title = item.text 
+                        # print(title)
+                        if (re.search('맥북', title)):
                                 print(title.strip())
-                                print('https://www.clien.net'  + item['href'])
+                        #         print('https://www.clien.net'  + item['href'])
                 except:
                         pass
         
